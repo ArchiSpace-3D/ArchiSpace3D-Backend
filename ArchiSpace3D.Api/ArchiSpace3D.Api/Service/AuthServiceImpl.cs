67d3bf72d5ee066185cbbdfe43095e9891ad4ce4
Angel Poveda <angeldanicp@gmail.com>
2026-09-09 10:17:36 -0500

    Limpieza total: se elimina todo el contenido de la rama

--- ArchiSpace3D.Api/ArchiSpace3D.Api/Service/AuthServiceImpl.cs
+++
@@ -1,9 +0,0 @@
-﻿using ArchiSpace3D.Api.Models;
-
-namespace ArchiSpace3D.Api.Service
-{
-    public interface AuthServiceImpl
-    {
-        Task<(string Token, Usuario Usuario)> LoginAsync(string email, string contrasena);
-    }
-}
--- Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace ArchiSpace3D.Api.Models
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
