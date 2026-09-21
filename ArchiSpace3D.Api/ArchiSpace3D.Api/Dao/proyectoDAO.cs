2865d211e933da1fa3360a1c76b1569802eb807e
Angel Poveda <angeldanicp@gmail.com>
2026-09-21 08:44:30 -0500

    fix(backend): fix 500 error when deleting project by manually cascading delete to related entities

--- ArchiSpace3D.Api/ArchiSpace3D.Api/Dao/proyectoDAO.cs
+++ ArchiSpace3D.Api/ArchiSpace3D.Api/Dao/proyectoDAO.cs
@@ -89,6 +89,31 @@ namespace ArchiSpace3D.Api.Dao
                 return false;
             }
 
+            // Eliminar dependencias manualmente para evitar el error de llave foránea (InternalServerError 500)
+            
+            var mediciones = _context.Medicions.Where(m => m.Idproyecto == id);
+            _context.Medicions.RemoveRange(mediciones);
+
+            var espacios = _context.Espaciofisicos.Where(e => e.Idproyecto == id);
+            _context.Espaciofisicos.RemoveRange(espacios);
+
+            var notificaciones = _context.Notificacions.Where(n => n.Idproyecto == id);
+            _context.Notificacions.RemoveRange(notificaciones);
+
+            var invitaciones = _context.Invitacions.Where(i => i.Idproyecto == id);
+            _context.Invitacions.RemoveRange(invitaciones);
+
+            var versiones = await _context.Versiondisenos.Where(v => v.Idproyecto == id).ToListAsync();
+            foreach (var v in versiones)
+            {
+                var elementos = _context.Elementoestructurals.Where(e => e.Idversiondiseno == v.Idversiondiseno);
+                _context.Elementoestructurals.RemoveRange(elementos);
+
+                var modelos = _context.Modeloimportados.Where(m => m.Idversiondiseno == v.Idversiondiseno);
+                _context.Modeloimportados.RemoveRange(modelos);
+            }
+            _context.Versiondisenos.RemoveRange(versiones);
+
             _context.Proyectos.Remove(existente);
             await _context.SaveChangesAsync();
             return true;
